a63462eed353238cdaf26c1162a4da827e2c6a6e
shuto niimi <71388489+ooitanojohn@users.noreply.github.com>
2021-03-06 06:15:50 +0900

    モバイルのデザイン scss htmlで書いた css適応されてない

--- html/wp-content/themes/theme-bones-master/home.php
+++ html/wp-content/themes/theme-bones-master/home.php
@@ -1,9 +1,69 @@
 <?php get_header(); ?>
 
 <div id="content">
+	<pre>
+		mobile design
+	</pre>
+	<ul>
+		<li><img src="../theme-bones-master/library/images/volare-hp-mobile1.png" alt="mobile-design"></li>
+		<li><img src="../theme-bones-master/library/images/volare-hp-mobile2.png" alt="mobile-design"></li>
+		<li><img src="../theme-bones-master/library/images/volare-hp-mobile3.png"></li>
+		<li><img src="../theme-bones-master/library/images/volare-hp-mobile4.png"></li>
+		<li><img src="../theme-bones-master/library/images/volare-hp-mobile5.png" alt="mobile-design"></li>
+	</ul>
+
 	<div id="inner-content">
-		<main id="main" >
-			トップページ
+		<main id="main">
+			<article id="top">
+				<div class="bg">
+					<div class="bg">
+						<h1>未来に羽ばたく学生エンジニア団体<span>VOLARE</span></h1>
+					</div>
+					<p>造りたい時に造れないをなくす</p>
+					<p>エンジニアにとって<strong>最高の遊び場を</strong></p>
+					<pre>volareは学生が好きなサービスを開発できるように設立された団体です。</pre>
+				</div>
+			</article>
+
+			<article id="mission">
+				<section class="bg">
+					<div class="none"></div>
+					<h3>MISSION</h3>
+					<pre>
+
+					</pre>
+				</section>
+				<!--↑ or ↓-->
+				<section>
+					<div class="hidden bg"></div>
+					<h3>MISSION</h3>
+					<pre>
+
+					</pre>
+				</section>
+				<section>
+
+				</section>
+			</article>
+
+			<article id="product">
+				<section class="bg">
+					<div class="none"></div>
+					<h3>プロダクト名</h3>
+					<p></p>
+					<button type="submit" class="">実際のサービスを見る</button>
+				</section>
+				<!--↑ or ↓-->
+				<section>
+					<div class="hidden bg"></div>
+					<h3>プロダクト名</h3>
+					<p></p>
+					<button type="submit" class="">実際のサービスを見る</button>
+				</section>
+				<section></section>
+			</article>
+
+
 		</main>
 	</div>
 </div>
--- html/wp-content/themes/theme-bones-master/library/config.rb
+++ html/wp-content/themes/theme-bones-master/library/config.rb
@@ -9,10 +9,10 @@
 http_path = "/"
 
 # 2. probably don't need to touch these
-css_dir = "../css"
-sass_dir = "./"
-images_dir = "../images"
-javascripts_dir = "../js"
+css_dir = "./css"
+sass_dir = "./scss"
+images_dir = "./images"
+javascripts_dir = "./js"
 environment = :development
 relative_assets = true
 
